62e7d8ff80e58e2d3668aa7da8717dad8c28625d
Swarit <boricpowderacid@gmail.com>
2025-02-28 10:37:27 +0530

    Created springboot maven proj

---
+++ src/test/java/com/example/ta1/Ta1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ta1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ta1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
