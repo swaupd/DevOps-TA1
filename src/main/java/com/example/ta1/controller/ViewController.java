94502aa09c6949465c5ba86a9fec6d0184d91d05
Swarit <boricpowderacid@gmail.com>
2025-02-28 11:04:51 +0530

    Added dynamic view controller to add any page in templates to a route

---
+++ src/main/java/com/example/ta1/controller/ViewController.java
@@ -0,0 +1,14 @@
+package com.example.ta1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/{page}")
+    public String servePage(@PathVariable String page) {
+        return page.toLowerCase(); // Serves templates/{page}.html
+    }
+}
